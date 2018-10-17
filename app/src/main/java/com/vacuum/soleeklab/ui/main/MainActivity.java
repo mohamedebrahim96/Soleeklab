f362a079a62136787317c612c5776e311e2c653d
Mohamed Ebrahim <ebrahimm131@gmail.com>
2018-10-17 13:52:27 +0200

    sign out Done & Login and Sign up

--- app/src/main/java/com/vacuum/soleeklab/ui/main/MainActivity.java
+++ app/src/main/java/com/vacuum/soleeklab/ui/main/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,28 +14,39 @@ import com.vacuum.soleeklab.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
-
+    private Button log_out;
+    private FirebaseAuth.AuthStateListener authListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signout();
+        initview();
     }
 
-    private void signout() {
+    private void initview() {
+        log_out =  findViewById(R.id.log_out);
         auth = FirebaseAuth.getInstance();
-        auth.signOut();
-
-// this listener will be called when there is change in firebase user session
-        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        authListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
+                //FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user == null) {
                     startActivity(new Intent(MainActivity.this, LoginActivity.class));
                     finish();
                 }
             }
         };
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signout();
+            }
+        });
+
+    }
+
+    private void signout() {
+        auth.signOut();
     }
 }
